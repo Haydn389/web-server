6843a385ff99a262aaa924be95ab95a98b19575e
Ubuntu <ubuntu@ip-172-31-12-78.ap-northeast-2.compute.internal>
2022-11-08 05:31:59 +0000

    complete tiny

---
+++ echo/echo.c
@@ -0,0 +1,22 @@
+#include "csapp.h"
+
+void echo(int connfd)
+{
+    size_t n;
+    char buf[MAXLINE];
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0)
+    {
+        printf("server received %d bytes\n", (int)n);
+        Rio_writen(connfd, buf, n);
+    }
+}
+
+void rio_readinitb(rio_t *rp, int fd) 
+{
+    rp->rio_fd = fd;  
+    rp->rio_cnt = 0;  
+    rp->rio_bufptr = rp->rio_buf;
+}
